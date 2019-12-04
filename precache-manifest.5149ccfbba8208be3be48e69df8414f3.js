f3f912010b58a391d24dadb11f020a273def412e
shrutivtu <shrutisaxena1596@gmail.com>
2019-12-04 15:55:40 +0530

    Updates

---
+++ precache-manifest.5149ccfbba8208be3be48e69df8414f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a3d6add417f581f02c38c1cc1a5eb3",
+    "url": "/deployfoodapp/index.html"
+  },
+  {
+    "revision": "3fcaeb5eb54c20ac15f4",
+    "url": "/deployfoodapp/static/css/main.d751bf55.chunk.css"
+  },
+  {
+    "revision": "5dded5d37412a7f85799",
+    "url": "/deployfoodapp/static/js/2.6942f18d.chunk.js"
+  },
+  {
+    "revision": "3fcaeb5eb54c20ac15f4",
+    "url": "/deployfoodapp/static/js/main.cb8af65e.chunk.js"
+  },
+  {
+    "revision": "9da583472a24e882d715",
+    "url": "/deployfoodapp/static/js/runtime-main.6c0d550f.js"
+  },
+  {
+    "revision": "35d9c3d78079e7790ae7a2dd679b3d84",
+    "url": "/deployfoodapp/static/media/foodimg1.35d9c3d7.jpeg"
+  }
+]);
